5c0c7d6f3fc7a4dd5d53b88c6ad7230d4b615557
Davi Ferreira <46787385+DaviRFerreir4@users.noreply.github.com>
2025-02-18 09:23:18 -0300

    Conexão com arquivo JSON e filtro
    Recuperando dados do arquivo JSON e filtrando os resultados de acordo com a entrada do usuário

--- Codecademy/global_modules.js
+++ Codecademy/global_modules.js
@@ -1,11 +1,36 @@
 import os from 'node:os';
-console.log(os.type());
+console.clear();
+console.log(`Você está utilizando o seguinte sistema operacional: ${os.type()}\n`);
+
+//---------------------------------------------------------------------
+
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
 const user = await rl.question('Usuário: ');
-const password = await rl.question('Senha: ');
+//const password = await rl.question('Senha: ');
 
 rl.close();
+
+//----------------------------------------------------------------------
+
+import { readFile } from 'node:fs/promises';
+const filePath = "data.json";
+
+// procurar um jeito de ler o arquivo e retornar somente os dados da pesquisa
+
+readFile(filePath, "utf-8").then(jsonRes => {
+    const dados = JSON.parse(jsonRes);
+    const usuarioLogado = dados.usuarios.filter((usuario => {
+        return (usuario.nome == user) ? usuario : false;
+    }));
+    if (usuarioLogado == "") {
+        console.log("\nErro: usuário não encontrado");
+    } else {
+        console.log(`\nUsuário encontrado!\nNome: ${usuarioLogado[0].nome}\nSenha: ${usuarioLogado[0].senha}\n`);
+    }
+}).catch(err => {
+    console.log(`Erro: ${err}`);
+})
