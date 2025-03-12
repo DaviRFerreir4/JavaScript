501b328e00c9d0d280c10ff807b6f0c8d7e11670
Davi Ferreira <46787385+DaviRFerreir4@users.noreply.github.com>
2025-03-12 11:53:19 -0300

    Organização das pastas e arquivos e inicio da funcionalidade de recuperação de senha

--- Personal projects/Login system/login.js
+++ Personal projects/Login system/login.js
@@ -1,7 +1,4 @@
-import os from 'node:os';
-// mostrando o sistema sendo utilizado
 console.clear();
-console.log(`Você está utilizando o seguinte sistema operacional: ${os.type()}\n`);
 
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
@@ -15,20 +12,21 @@ const rl = readline.createInterface({ input, output });
 // loop para aceitar somente as respostas pedidas
 var answer;
 do {
-    answer = await rl.question("Você já possui um usuário? (S/N): ");
+    answer = await rl.question("Você já possui um usuário? (S/N): ").then(res => {return res.toLowerCase(); });
     switch(answer) {
-        case "S":
         case "s":
-        case "N":
+            answer = true;
+            break;
         case "n":
-            console.clear();
+            answer = false;
             break;
         default:
             console.clear();
             console.log("Digite um dos valores pedidos!");
     }
-} while (answer != "S" && answer != "s" && answer != "N" && answer != "n");
-if (answer == "S" || answer == "s") {
+} while (typeof(answer) == "string");
+console.clear();
+if (answer) {
     const user = await rl.question('Usuário: ');
     const pass = await rl.question('Senha: ');
 
@@ -39,12 +37,31 @@ if (answer == "S" || answer == "s") {
             console.log("Erro: Usuário não encontrado. Confira seus dados e tente novamente ou cadastre-se");
         } else {
             // checando se a senha passada pelo usuário bate com o banco de dados
-            chkPass(pass, userRes[0].senha).then(res => {
+            chkPass(pass, userRes[0].senha).then(async res => {
                 if (res) {
                     console.log(`Usuário encontrado. Bem vindo ${userRes[0].nome}`);
                 } else {
-                    console.log("Erro: Senha incorreta. Confira seus dados e tente novamente");
-                    process.exit(1);
+                    do {
+                        answer = await rl.question("Erro: Senha incorreta. Deseja recuperar sua senha? (S/N): ");
+                        switch(answer) {
+                            case "s":
+                                answer = true;
+                                break;
+                            case "n":
+                                answer = false;
+                                break;
+                            default:
+                                console.clear();
+                                console.log("Digite um dos valores pedidos!");
+                        }
+                    } while (typeof(answer) == "string");
+                    if (answer) {
+                        console.log("Vai ficar querendo haha");
+                        process.exit(1);
+                    } else {
+                        console.log("Revise seus dados e tente novamente.");
+                        process.exit(1);
+                    }
                 }
             }).catch(err => {
                 console.log(err);
@@ -57,14 +74,14 @@ if (answer == "S" || answer == "s") {
     })
     // linha de código fora da promise para exemplificar a assincronidade do programa
     console.log("Buscando usuário, por favor aguarde...\n");
-} else if (answer == "N" || answer == "n") {
+} else {
     const newUser = await rl.question("Criando um usuário novo...\n\nDigite seu usuário: ");
     const newPass = await rl.question("Crie uma senha para seu usuário: ");
 
     rl.close();
 
     // inserindo o novo registro no arquivo
-    wrtUser(newUser, newPass).then(res => {
+    wrtUser(newUser, await hashPass(newPass)).then(res => {
         if (res === undefined) {
             console.log("Novo usuário registrado com sucesso!");
         } else if (res == "igual") {
@@ -87,5 +104,4 @@ if (answer == "S" || answer == "s") {
     próximas funcionalidades
         - verificação de duas etapas
         - funcionalidade de recuperação de senha
-        - bloquear a inserção de dados repetidos
 */
